3304303e13936ff001d9007665d7ceaeafd45617
webtutsdev <webdevtutsblog@gmail.com>
2016-12-01 21:27:16 +0530

    Create index.php

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<?php
+
+  $site_title = "PHP Project";
+  $site_text = "A PHP Project Hosted on GitHub!";
+
+?>
+
+<html>
+  <head>
+    <title><?= $site_title ?></title>
+    <style>
+      h1 {
+        text-align: center;
+      }
+    </style>
+  </head>
+  <body>
+    <h1><?= $site_text ?></h1>
+  </body>
+</html>
